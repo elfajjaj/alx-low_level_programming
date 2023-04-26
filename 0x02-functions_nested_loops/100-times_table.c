750ade7bc2917fc9428cef55d32edd67292f4d25
Fatima Ezzahra <fatimaezzahra@gmail.com>
2023-04-26 15:02:02 -0700

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,14 +25,14 @@ if (prod <= 9)
 _putchar(' ');
 if (prod >= 100)
 {
-_putchar((prod / 100) +'0');
-_putchar(((prod / 10)) % 10 +'0');
+_putchar((prod / 100) + '0');
+_putchar(((prod / 10)) % 10 + '0');
 }
 else if (prod <= 99 && prod >= 10)
 {
-_putchar((prod / 10) +'0');
+_putchar((prod / 10) + '0');
 }
-_putchar((prod % 10) +'0');
+_putchar((prod % 10) + '0');
 }
 _putchar('\n');
 }
